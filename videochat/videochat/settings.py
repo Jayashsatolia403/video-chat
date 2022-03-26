33eacbb0e37b401dd4c522122d8ce3694383a6e4
Jayash Satolia <jayash.satolia.min19@iitbhu.ac.in>
2022-03-26 12:34:21 +0530

    Update

--- videochat/videochat/settings.py
+++ videochat/videochat/settings.py
@@ -75,16 +75,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'videochat.wsgi.application'
 ASGI_APPLICATION = 'videochat.asgi.application'
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             "hosts": [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
 }
-
-
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
